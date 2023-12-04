967c20e410b59eeeeea76ada75e2b5e5eea276c2
James Mburu <mburuwanjiru364@gmail.com>
2023-12-04 23:52:43 +0300

    04-12-2023

---
+++ airestrip-system/php.php
@@ -0,0 +1,29 @@
+<?php
+
+class Runway {
+    public $id;
+    public $length;
+    public $width;
+
+    public function __construct($id, $length, $width) {
+        $this->id = $id;
+        $this->length = $length;
+        $this->width = $width;
+    }
+
+    public function displayDetails() {
+        echo "Runway $this->id - Length: $this->length, Width: $this->width<br>";
+    }
+}
+
+$runways = [
+    new Runway(1, 3000, 50),
+    // Add more runways as needed
+];
+
+echo '<h2>Runways</h2>';
+foreach ($runways as $runway) {
+    $runway->displayDetails();
+}
+
+?>
